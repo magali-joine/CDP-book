bcb0507240dcd1d5f05c1047a56b586f523e7e37
mjoyner <mjoyner@vbservices.net>
2014-11-22 19:46:32 -0500

    pronunciation entry validation stuff

--- CDP-book/src/net/cherokeedictionary/lyx/LyxExportFile.java
+++ CDP-book/src/net/cherokeedictionary/lyx/LyxExportFile.java
@@ -7,22 +7,24 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import net.cherokeedictionary.db.Db;
 import net.cherokeedictionary.main.DbEntry;
-import net.cherokeedictionary.main.JsonConverter;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
 
 public class LyxExportFile extends Thread {
-	
+
 	private final Db dbc;
 	private final String lyxfile;
 
 	public LyxExportFile(Db dbc, String lyxfile) {
-		this.dbc=dbc;
-		this.lyxfile=lyxfile;
+		this.dbc = dbc;
+		this.lyxfile = lyxfile;
 	}
 
 	@Override
@@ -35,13 +37,81 @@ public class LyxExportFile extends Thread {
 	}
 
 	public void _run() throws IOException {
-		StringBuilder lyxdoc=new StringBuilder();		
-		String start=IOUtils.toString(getClass().getResourceAsStream("/net/cherokeedictionary/lyx/LyxDocumentStart.txt"));
+		StringBuilder lyxdoc = new StringBuilder();
+		String start = IOUtils.toString(getClass().getResourceAsStream(
+				"/net/cherokeedictionary/lyx/LyxDocumentStart.txt"));
 		lyxdoc.append(start);
-		String end=IOUtils.toString(getClass().getResourceAsStream("/net/cherokeedictionary/lyx/LyxDocumentEnd.txt"));
-		
+		String end = IOUtils.toString(getClass().getResourceAsStream(
+				"/net/cherokeedictionary/lyx/LyxDocumentEnd.txt"));
 		List<DbEntry> entries = getEntries();
-		
+		removeUnwantedEntries(entries);
+		fixupPronunciations(entries);
+	}
+
+	private void fixupPronunciations(List<DbEntry> entries) {
+		Iterator<DbEntry> ientry = entries.iterator();
+		while (ientry.hasNext()) {
+			DbEntry entry = ientry.next();
+			System.out.println("Fixing Tones: "+entry.entrya+"="+entry.definitiond);
+			entry.entrytone=fixToneCadenceMarks(entry.entrytone);
+			entry.nounadjpluraltone=fixToneCadenceMarks(entry.nounadjpluraltone);
+			entry.vfirstprestone=fixToneCadenceMarks(entry.vfirstprestone);
+			entry.vsecondimpertone=fixToneCadenceMarks(entry.vsecondimpertone);
+			entry.vthirdinftone=fixToneCadenceMarks(entry.vthirdinftone);
+			entry.vthirdpasttone=fixToneCadenceMarks(entry.vthirdpasttone);
+			entry.vthirdprestone=fixToneCadenceMarks(entry.vthirdprestone);
+		}
+	}
+
+	private final String[] searchList={"?", "A.", "E.", "I.", "O.", "U.", "V.", "a.", "e.", "i.", "o.", "u.", "v.", "1", "2", "3", "4"};
+	private final String[] replacementList={"ɂ", "̣A", "̣E", "Ị", "Ọ", "Ụ", "Ṿ", "ạ", "ẹ", "ị", "ọ", "ụ", "ṿ", "¹", "²", "³", "⁴"};
+	private String fixToneCadenceMarks(String entrytone) {
+		String result=StringUtils.replaceEach(entrytone, searchList, replacementList);
+		if (result.matches(".*"+Pattern.quote(".")+".*")) {
+			System.err.println("\tBAD PRONUNCIATION ENTRY: "+entrytone+" => "+result);
+		}
+		if (result.matches(".*\\d.*")) {
+			System.err.println("\tBAD PRONUNCIATION ENTRY: "+entrytone+" => "+result);
+		}
+		if (result.matches(".*¹(¹²³⁴).*")) {
+			System.err.println("\tBAD PRONUNCIATION ENTRY: "+entrytone+" => "+result);
+		}
+		if (result.matches(".*⁴(¹²³⁴).*")) {
+			System.err.println("\tBAD PRONUNCIATION ENTRY: "+entrytone+" => "+result);
+		}
+		if (result.matches(".*²(¹²⁴).*")) {
+			System.err.println("\tBAD PRONUNCIATION ENTRY: "+entrytone+" => "+result);
+		}
+		if (result.matches(".*³(¹³⁴).*")) {
+			System.err.println("\tBAD PRONUNCIATION ENTRY: "+entrytone+" => "+result);
+		}
+		return result;
+	}
+
+	/**
+	 * We don't want "empty", "word parts", or
+	 * "Cross references to the not-included Grammar"
+	 * 
+	 * @param entries
+	 */
+	private void removeUnwantedEntries(List<DbEntry> entries) {
+		Iterator<DbEntry> ientry = entries.iterator();
+		while (ientry.hasNext()) {
+			DbEntry entry = ientry.next();
+			if (StringUtils.isEmpty(entry.syllabaryb)) {
+				ientry.remove();
+				continue;
+			}
+			if (entry.syllabaryb.contains("-")) {
+				ientry.remove();
+				continue;
+			}
+			if (entry.definitiond.contains("(see Gram. ")) {
+				ientry.remove();
+				continue;
+			}
+		}
+
 	}
 
 	private List<DbEntry> getEntries() {
@@ -51,7 +121,7 @@ public class LyxExportFile extends Thread {
 			ResultSet rs = s.executeQuery("select * from likespreadsheets");
 			while (rs.next()) {
 				DbEntry entry = new DbEntry();
-				for(Field f: DbEntry.class.getFields()) {
+				for (Field f : DbEntry.class.getFields()) {
 					String simpleName = f.getType().getSimpleName();
 					if (simpleName.equals("String")) {
 						String name = f.getName();
@@ -59,7 +129,6 @@ public class LyxExportFile extends Thread {
 					}
 				}
 				list.add(entry);
-				System.out.println(new JsonConverter().toJson(entry));
 			}
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
