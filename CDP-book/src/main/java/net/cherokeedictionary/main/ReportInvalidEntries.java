3435707d38ef78e335b4dc2e0f661e54d12f5557
mjoyner <mjoyner@vbservices.net>
2016-02-17 22:21:32 -0500

    error detection and reporting against live db

---
+++ CDP-book/src/main/java/net/cherokeedictionary/main/ReportInvalidEntries.java
@@ -0,0 +1,121 @@
+package net.cherokeedictionary.main;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+import java.util.TimeZone;
+
+import org.apache.commons.io.FileUtils;
+
+import net.cherokeedictionary.dao.DaoCherokeeDictionary;
+import net.cherokeedictionary.model.DictionaryEntry;
+import net.cherokeedictionary.model.DictionaryEntry.Crossreference;
+import net.cherokeedictionary.model.DictionaryEntry.EntryExample;
+import net.cherokeedictionary.model.DictionaryEntry.EntryForm;
+import net.cherokeedictionary.model.DictionaryEntry.EntryFormType;
+import net.cherokeedictionary.model.DictionaryEntry.Note;
+import net.cherokeedictionary.model.LikeSpreadsheetsRecord;
+import net.cherokeedictionary.shared.DictionaryEntryValidator;
+
+public class ReportInvalidEntries {
+
+	private final DaoCherokeeDictionary dao = DaoCherokeeDictionary.dao;
+	
+	public ReportInvalidEntries(String reportfile_html) {
+		List<DictionaryEntry> entries = getRecords();
+		
+		StringBuilder html_head=new StringBuilder();
+		
+		DateFormat sdf = SimpleDateFormat.getDateTimeInstance();
+		sdf.setTimeZone(TimeZone.getTimeZone("EST5EDT"));
+		
+		html_head.append("<html><head>");
+		html_head.append("<meta charset=\"UTF-8\" lang=\"chr\" />");
+		html_head.append("<title>Invalid entries for cherokeedictonary.net</title>");
+		html_head.append("<style>\n");
+		html_head.append("@import url(http://fonts.googleapis.com/earlyaccess/notosanscherokee.css);\n");
+		html_head.append("body {font-family: 'Noto Sans Cherokee', sans-serif;}\n");
+		html_head.append("</style>\n");
+		html_head.append("</head>");
+		html_head.append("<body>\n");
+		StringBuilder html_body=new StringBuilder();
+		
+		Iterator<DictionaryEntry> ientry = entries.iterator();
+		int invalid=0;
+		System.out.println("\tScanning.");
+		while (ientry.hasNext()) {
+			DictionaryEntryValidator entry = new DictionaryEntryValidator(ientry.next());
+			entry.validate();
+			if (!entry.isValid()) {
+				invalid++;
+				html_body.append("<a id='_");
+				html_body.append(invalid);
+				html_body.append("'></a>");
+				html_body.append(String.format("[%04d]\n", invalid));
+				html_body.append("<pre>");
+				html_body.append(entry.simpleFormatted());
+				html_body.append("\n");
+//				String syllabary = entry.forms.get(0).syllabary;
+//				html_body.append(Syllabary.asLatinMatchPattern(syllabary));
+//				html_body.append("\n");
+				html_body.append("</pre>");
+				continue;
+			}
+		}
+		
+		html_head.append("<p>=== A total of ");
+		html_head.append(invalid);
+		html_head.append(" entries with errors were detected.</p>");
+		
+		html_head.append("<p>Last updated: ");
+		html_head.append(sdf.format(new Date()));
+		html_head.append("</p>");
+		
+		System.out.println("\tWriting report to: "+reportfile_html);
+		
+		html_body.append("<p>");
+		html_body.append("<em>Just because an entry fails simple validation does not make it wrong.</em>");
+		html_body.append("</p>");
+		html_body.append("\n</body></html>\n");
+		
+		try {
+			FileUtils.write(new File(reportfile_html), html_head.toString()+html_body.toString(), "UTF-8");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private List<DictionaryEntry> getRecords(){
+		List<DictionaryEntry> newRecords = new ArrayList<>();
+		List<LikeSpreadsheetsRecord> oldRecords = dao.getLikespreadsheetRecords("CED");
+		System.out.println("Loaded "+oldRecords.size()+" records for scanning.");
+		for (LikeSpreadsheetsRecord old: oldRecords) {
+			DictionaryEntry entry = new DictionaryEntry();
+			entry.id=old.id;
+			entry.crossreferences.add(new Crossreference(old.crossreferencet));
+			entry.definitions.add(old.definitiond);
+			entry.examples.add(new EntryExample(old.sentencesyllr, old.sentenceq, old.sentenceenglishs));
+			entry.forms.add(new EntryForm(EntryFormType.Verb3rdPrc,old.syllabaryb, old.entrytone, old.entrya));
+			entry.forms.add(new EntryForm(EntryFormType.Verb1stPrc, old.vfirstpresh, old.vfirstprestone, old.vfirstpresg));
+			entry.forms.add(new EntryForm(EntryFormType.Verb3rdPast,old.vthirdpastsyllj, old.vthirdpasttone, old.vthirdpasti));
+			entry.forms.add(new EntryForm(EntryFormType.Verb3rdHab,old.vthirdpressylll, old.vthirdprestone, old.vthirdpresk));
+			entry.forms.add(new EntryForm(EntryFormType.Verb2ndImp,old.vsecondimpersylln, old.vsecondimpertone, old.vsecondimperm));
+			entry.forms.add(new EntryForm(EntryFormType.Verb3rdInf,old.vthirdinfsyllp, old.vthirdinftone, old.vthirdinfo));
+			entry.forms.add(new EntryForm(EntryFormType.Other,old.nounadjpluralsyllf, old.nounadjpluraltone, old.nounadjplurale));
+			entry.notes.add(new Note(old.notes));
+			entry.pos=old.partofspeechc;
+			entry.source=old.source;
+			newRecords.add(entry);
+		}
+		Collections.sort(newRecords);
+		System.out.println("\tReformatted.");
+		return newRecords;
+	}
+
+}
---
+++ CDP-book/src/main/java/net/cherokeedictionary/shared/DictionaryEntryValidator.java
@@ -0,0 +1,247 @@
+package net.cherokeedictionary.shared;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.cherokeelessons.chr.Syllabary;
+
+import net.cherokeedictionary.model.DictionaryEntry;
+
+public class DictionaryEntryValidator extends DictionaryEntry {
+
+	public DictionaryEntryValidator(DictionaryEntry entry) {
+		super(entry);
+	}
+
+	public DictionaryEntryValidator() {
+	}
+
+	private boolean valid = true;
+	private Set<String> errors = new HashSet<>();
+
+	private void clean() {
+		valid = true;
+		int size_syllabary = 0;
+		int size_pronunciation = 0;
+		for (int ix = 0; ix < forms.size(); ix++) {
+			EntryForm form = forms.get(ix);
+			String p = form.pronunciation;// pronunciations.get(ix);
+			String s = form.pronunciation;// syllabary.get(ix);
+			if (p.replaceAll("[ -]+", "").isEmpty()) {
+				p = "";
+				form.pronunciation = "";// pronunciations.set(ix, "");
+			} else {
+				size_pronunciation++;
+			}
+			if (s.replaceAll("[ -]+", "").isEmpty()) {
+				s = "";
+				form.syllabary = "";// syllabary.set(ix, "");
+			} else {
+				size_syllabary++;
+			}
+		}
+	}
+
+	public boolean isValid() {
+		if (!valid) {
+			return false;
+		}
+		for (int ix = 0; ix < forms.size(); ix++) {
+			EntryForm form = forms.get(ix);
+			String s = form.syllabary;
+			String p = form.pronunciation;
+			if (s.isEmpty() != p.isEmpty()) {
+				if (s.isEmpty() && (p.startsWith("-") || p.endsWith("-"))) {
+					continue;
+				}
+				valid = false;
+				if (s.isEmpty()) {
+					form.syllabary = "***";
+					errors.add("Missing Syllabary Entry");
+				}
+				if (p.isEmpty()) {
+					form.pronunciation = "***";
+					errors.add("Missing Pronunciation Entry");
+				}
+				continue;
+			}
+			if (s.isEmpty()) {
+				continue;
+			}
+			if (p.matches(".*[¹²³⁴]{1,2}[^aeiouvạẹịọụṿ¹²³⁴]+[¹²³⁴]{1,2}.*")) {
+				valid = false;
+				form.pronunciation = "*** " + p;
+				errors.add("Bad Tone Mark/Missing a Vowel");
+				continue;
+			}
+			if (!s.replaceAll("[Ꭰ-Ᏼ ,\\-]+", "").isEmpty()) {
+				form.syllabary = "*** " + s;
+				valid = false;
+				errors.add("Bad Syllabary Entry");
+				continue;
+			}
+			if (s.length() > 1 && !p.matches(".*?[¹²³⁴].*?")) {
+				System.err.println("\t"+s+" ["+p+"]");
+			}
+			if (s.startsWith("-") && !p.startsWith("-")){
+				valid = false;
+				form.pronunciation = "*** " + p;
+				errors.add("Missing hyphen?");
+				continue;
+			}
+			if (s.endsWith("-") && !p.endsWith("-")){
+				valid = false;
+				form.pronunciation = "*** " + p;
+				errors.add("Missing hyphen?");
+				continue;
+			}
+			if (p.startsWith("-") && !s.startsWith("-")){
+				valid = false;
+				form.syllabary = "*** " + s;
+				errors.add("Missing hyphen?");
+				continue;
+			}
+			if (p.endsWith("-") && !s.endsWith("-")){
+				valid = false;
+				form.syllabary = "*** " + s;
+				errors.add("Missing hyphen?");
+				continue;
+			}
+//			if (!s.startsWith("-") && !s.endsWith("-")){
+				if (s.replaceAll("[^Ꭰ-Ᏼ]", "").length() > 1 && !p.matches(".*?[¹²³⁴].*?")) {
+					valid = false;
+					form.pronunciation = "*** " + p;
+					errors.add("Invalid Pronunciation Entry");
+					continue;
+				}
+//			}
+			if (s.contains(",") || p.contains(",")) {
+				int scount = 0;
+				int is = -1;
+				while ((is = s.indexOf(",", is + 1)) >= 0) {
+					scount++;
+				}
+				;
+				int ip = -1;
+				int pcount = 0;
+				while ((ip = p.indexOf(",", ip + 1)) >= 0) {
+					pcount++;
+				}
+				;
+				if (scount != pcount) {
+					valid = false;
+					errors.add("Commas Mismatch");
+					form.syllabary = "*** [" + scount + "] " + s;
+					form.pronunciation = "*** [" + pcount + "] " + p;
+					continue;
+				}
+			}
+			if (!passesSyllabaryMatch(s, p)) {
+				errors.add("Syllabary and Pronunciation Disagree");
+				form.syllabary = "*** " + s;
+				valid = false;
+				continue;
+			}
+		}
+
+		return valid;
+	}
+
+	private boolean passesSyllabaryMatch(String s, String p) {
+		// System.out.println(s+" "+Syllabary.asLatinMatchPattern(s));
+		return p.toLowerCase().matches(Syllabary.asLatinMatchPattern(s));
+	}
+
+	public String simpleFormatted() {
+		EntryForm form = forms.get(0);
+		StringBuilder sb = new StringBuilder();
+		sb.append("id: " + id);
+		if (errors.size() > 0) {
+			sb.append(" ");
+			sb.append(errors);
+		}
+		sb.append("\n");
+		sb.append(form.syllabary);
+		sb.append(" [");
+		sb.append(form.pronunciation);
+		sb.append("] ");
+		sb.append("\u201C");
+		sb.append(this.definitions.get(0));
+		sb.append("\u201D");
+		sb.append("\n");
+		for (int ix = 1; ix < forms.size(); ix++) {
+			EntryForm subform = forms.get(ix);
+			String s = subform.syllabary;
+			String p = subform.pronunciation;
+			if (s.isEmpty() && p.isEmpty()) {
+				continue;
+			}
+			sb.append("    ");
+			sb.append(s);
+			sb.append(" [");
+			sb.append(p);
+			sb.append("] ");
+			sb.append("\n");
+		}
+		return sb.toString();
+	}
+
+	private void splitCombinations() {
+		Iterator<EntryForm> iform = forms.iterator();
+		while (iform.hasNext()) {
+			EntryForm form = iform.next();
+			if (form.syllabary.contains(",") != form.pronunciation.contains(",")) {
+				valid = false;
+			}
+		}
+	}
+
+	public void validate() {
+		noNullsOnValidatedFields();
+		fixPronunciations();
+		splitCombinations();
+		clean();
+	}
+
+	private void fixPronunciations() {
+		forms.forEach(f -> f.pronunciation = fixPronunciation(f.pronunciation));
+	}
+
+	private static final String[] searchList = { "?", "A.", "E.", "I.", "O.", "U.", "V.", "a.", "e.", "i.", "o.", "u.",
+			"v.", "1", "2", "3", "4" };
+	private static final String[] replacementList = { "ɂ", "̣A", "̣E", "Ị", "Ọ", "Ụ", "Ṿ", "ạ", "ẹ", "ị", "ọ", "ụ", "ṿ",
+			"¹", "²", "³", "⁴" };
+
+	private static String fixPronunciation(String pronounce) {
+		pronounce = StringUtils.replaceEach(pronounce, searchList, replacementList);
+		return pronounce;
+	}
+
+	private void noNullsOnValidatedFields() {
+		this.crossreferences.removeIf(cr -> cr == null);
+		this.definitions.removeIf(d -> d == null);
+		this.examples.removeIf(e -> e == null);
+		this.forms.removeIf(f -> f == null);
+		this.notes.removeIf(n -> n == null);
+		this.crossreferences.forEach(c -> {
+			c.crossReference = StringUtils.defaultString(c.crossReference);
+		});
+		this.forms.forEach(f -> {
+			f.formType = f.formType == null ? EntryFormType.Other : f.formType;
+			f.latin = f.latin == null ? "" : f.latin;
+			f.pronunciation = f.pronunciation == null ? "" : f.pronunciation;
+			f.syllabary = f.syllabary == null ? "" : f.syllabary;
+		});
+		this.examples.forEach(e -> {
+			e.english = e.english == null ? "" : e.english;
+			e.latin = e.latin == null ? "" : e.latin;
+			e.syllabary = e.syllabary == null ? "" : e.syllabary;
+		});
+		this.notes.forEach(n -> {
+			n.note = n.note == null ? "" : n.note;
+		});
+	}
+}
